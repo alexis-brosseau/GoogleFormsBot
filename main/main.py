b6ad40eaf01d82d90e6eafa33e981db72557c964
Shawey <42684222+Shawey@users.noreply.github.com>
2021-12-13 01:06:43 -0500

    GoogleFormsBot v1.1.0
    You can now use proxies.

--- main/main.py
+++ main/main.py
@@ -1,28 +1,47 @@
-import requests, threading
+import requests, threading, os, random
+
+from pathlib import Path
+cPath = Path(os.path.realpath(__file__))
+ePath = Path(os.path.realpath(__file__)).parents[1]
 
 n = 0
-threadLock = threading.Lock()
+nPrint = 0
 
-def check(awnsersNum, url, formData):
+def check(awnsersNum, url, formData, proxyList):
     global n
     
     while awnsersNum == 0:
-        sendRequests(url, formData)
+        sendRequests(url, formData, proxyList)
     else:
         while n < awnsersNum:
-            sendRequests(url, formData)
+            sendRequests(url, formData, proxyList)
+    with threading.Lock():
+        n += 1
     return()
 
-def sendRequests(url, formData):
+def sendRequests(url, formData, proxyList):
     global n
+    global nPrint
+    proxy = proxyList[random.randrange(len(proxyList))]
     
-    with threadLock:
+    with threading.Lock():
         n += 1
-        print('  %d awnsers sent' %(n))
-    requests.post(url, allow_redirects=False, data=formData)
 
-def run(awnsersNum, threadsNum, url, formData):
-    threads = [threading.Thread(target=check, args=(awnsersNum, url, formData,), daemon = True) for t in range(threadsNum)]
+    try:
+        requests.get(url, proxies={'http': 'http://'+proxy, 'https': 'http://'+proxy,}, allow_redirects=False, data=formData,)
+        nPrint += 1
+        print('  %s awnsers sent' %(nPrint))
+    except:
+        requests.post(url, allow_redirects=False, data=formData)
+        nPrint += 1
+        if proxy == '00.00.00.00:0000':
+            print('  %s awnsers sent' %(nPrint))
+        else:
+            print('  %s awnsers sent [Sended without proxy because %s failed]' %(nPrint, proxy))
+    return()
+
+def run(awnsersNum, threadsNum, url, formData, proxyList):
+    threads = [threading.Thread(target=check, args=(awnsersNum, url, formData, proxyList,), daemon = True) for t in range(threadsNum)]
     
     for t in threads:
         t.start()
--- main/run.py
+++ main/run.py
@@ -1,10 +1,10 @@
 import os, configparser, ast
 
 from pathlib import Path
-cPath = Path(os.path.realpath(__file__)).parents[1]
+ePath = Path(os.path.realpath(__file__)).parents[1]
 
 config = configparser.ConfigParser()
-config.read(cPath / 'config.ini')
+config.read(ePath / 'config.ini')
 url = config.get('DEFAULT', 'url')
 formData = ast.literal_eval(config.get('DEFAULT', 'formData'))
 
@@ -16,24 +16,48 @@ print ('''
    /_/   /_/\__,_/____/____/\__, /  
                            /____/   
 
-      Google Forms Bot v1.0.0
+      Google Forms Bot v1.1.0
         https://flassy.xyz/\n''')
+
+proxies = input('• Do you want to use proxies? [y/n] :\n» ')
+if proxies == 'y':
+  i = 0
+  pl=[]
+  
+  pf_size = os.stat(ePath / 'proxylist.txt').st_size
+  if pf_size == 0:
+    input('\n• No proxies were found in the file, press enter to exit :\n» ')
+    quit()
+  else:
+    pf = open(ePath / 'proxylist.txt', 'r')
+    
+    for line in pf:
+      pl.insert(0, line)
+      i += 1
+    pl = [item.replace('\n', '') for item in pl]
+elif proxies == 'n':
+  pl = ['00.00.00.00:0000']
+else:
+  quit()
 
 awnsersNum = int(input('\n• How many awnser do you want to send? (0 will send requests indefinitely) :\n» '))
 
-threadsNum = int(input('''
-• How many threads do you want to use? :
+if proxies == 'n':
+  threadsNum = int(input('''
+  • How many threads do you want to use? :
 
-┍━━ WARNING ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-  Google may block temporarily your requests if you send awnsers too quickly 
-  1 thread: no chance of getting blocked                                     
-  2 threads: some chance of getting blocked afer a while                     
-  3+ threads: high chance of getting blocked afer a while                                
-┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
-\n» '''))
+  ┍━━ WARNING ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+    Google may block you from sending requests if you send them too quickly
+    over an extended period of time. In that case, the recommended number of
+    threads is 1 or 2. Consider using some proxies to avoid getting blocked.
+  ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
+  \n» '''))
+else:
+  threadsNum = int(input('\n• How many threads do you want to use? :\n» '))
+
 print('\n• Starting...')
 
 from main import run
-run(awnsersNum, threadsNum, url, formData)
-input('• Finished sending awnsers, press enter to exit')
+run(awnsersNum, threadsNum, url, formData, pl)
+input('• Finished sending awnsers, press enter to exit :\n» ')
 quit()
